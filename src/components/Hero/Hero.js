f8990fcbf1d37934f66f1664b34f8e8475c734ec
Alejandro Lete <alete8@gmail.com>
2022-05-09 14:53:52 -0300

    add styled-components

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
 const Hero = () => {
-  return <div>Aca vamos a desarrollar la seccion Hero</div>;
+  return (
+    <StyledContainer>Aca vamos a desarrollar la seccion Hero</StyledContainer>
+  );
 };
 
 export default Hero;
+
+const StyledContainer = styled.div`
+  display: flex;
+  background-color: yellow;
+`;
