faf7fe314c49087aad2315a86df2de96057bebfb
Farvon <farvon@gmail.com>
2022-05-09 23:25:21 -0300

    POK1 - Header & Hero

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
+
+
+
+const HeaderLogo = () => {
+  const poke = 'https://cdn-icons-png.flaticon.com/512/361/361998.png';
+  return (
+    <a className='header-logo-conteiner' href="/">
+      <img className="pokeIcon" src={poke} />
+      <p>Pokedex</p>
+    </a>
+  );
+};
+
+const HeaderNav = () => {
+  return (
+    <nav className="header-nav">
+      
+      <Link to="#home" className='header-nav-link'>Home</Link>
+
+      <Link to="#history" className='header-nav-link'>History</Link>
+
+      <Link to="#Video" className='header-nav-link'>Video</Link>
+
+      <Link to="#contact" className='header-nav-link'>Contact</Link>
+
+      <Link to="pokedex" className='header-nav-link'>Pokedex</Link>
+
+      <Link to="my-favs" className='header-nav-link'>MyFavs</Link>
+    </nav>
+  );
+};
 
 const Header = () => {
   return (
-    <div>
-      Aca vamos a desarrollar el Header
-      <Link to='pokedex'>Pokedex</Link>
-      <Link to='my-favs'>MyFavs</Link>
-    </div>
+    <header className="header">
+
+      <HeaderLogo />
+
+      <HeaderNav />
+
+      
+    </header>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import './Hero.css';
+
+const pikachuImg = require.context('../../assets', true);
 
 const Hero = () => {
-  return <div>Aca vamos a desarrollar la seccion Hero</div>;
+  return (
+    <div>
+    <div className="home">
+      <div className="homeImg">
+        <img
+          src={pikachuImg('./heroPikachu.png')}
+          className="homeImgPikachu"
+        ></img>
+      </div>
+      <div id="home" className="homeWelcome">
+        <h1>Welcome to Pokedex</h1>
+        <p>Join our comunity to find Pokémon's news</p>
+        <button className="homeButton">View more</button>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default Hero;
