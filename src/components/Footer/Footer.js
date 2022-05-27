39a6fbb068a58bd4ab9695b5120de95e8c61701c
Alejandro Lete <alete8@gmail.com>
2022-05-27 11:52:57 -0300

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -33,8 +33,6 @@ const FooterContainer = styled.footer`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  position: fixed;
-  bottom: 0;
   width: 100%;
   height: 50px;
   background-color: gray;
