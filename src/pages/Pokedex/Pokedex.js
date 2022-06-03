77822e5a2f810fdf54c3ea71f76383d41639fbc5
Alejandro Lete <alete8@gmail.com>
2022-06-03 16:33:55 -0300

    POK27 - changes in filters

--- src/pages/Pokedex/Pokedex.js
+++ src/pages/Pokedex/Pokedex.js
@@ -36,11 +36,11 @@ const Pokedex = () => {
     setFavorites(pokemonsFiltered);
   };
 
-
-
   const handleOptionSelected = (option) => {
+    let filteredPokemons = [...pokemons];
+
     if (option.value === 1) {
-      pokemons.sort((a, b) => {
+      filteredPokemons.sort((a, b) => {
         if (a.id > b.id) {
           return 1;
         } else {
@@ -51,12 +51,10 @@ const Pokedex = () => {
           }
         }
       });
-      console.log('1');
-      console.log(pokemons);
-      setPokemons(pokemons);
+      setFavorites(filteredPokemons);
     } else {
       if (option.value === 2) {
-        pokemons.sort((a, b) => {
+        filteredPokemons.sort((a, b) => {
           if (a.name.toLowerCase() > b.name.toLowerCase()) {
             return 1;
           } else {
@@ -67,37 +65,33 @@ const Pokedex = () => {
             }
           }
         });
-        console.log('2');
-        console.log(pokemons);
-        setPokemons(pokemons);
+        setFavorites(filteredPokemons);
       }
     }
   };
-
-  let onMyFavsSection = false;
 
   return (
     <PokedexContainer>
       <Header />
       <PokeSearchContainer>
         <PokeSearchBar onSearch={(e) => filterPokemon(e)} />
-        <FilterMenu
-          options={[
-            { label: 'Pokedex Id', value: 1 },
-            { label: 'A-Z', value: 2 },
-            { label: 'Type', value: 3 },
-          ]}
-          onOptionSelected={(e) => handleOptionSelected(e)}
-        />
+        <FilterContainer>
+          <FilterText>Sort by</FilterText>
+          <FilterMenu
+            options={[
+              { label: 'Pokedex No.', value: 1 },
+              { label: 'A-Z', value: 2 },
+              { label: 'Type', value: 3 },
+            ]}
+            onOptionSelected={(e) => handleOptionSelected(e)}
+          />
+        </FilterContainer>
       </PokeSearchContainer>
       <PokedexCardsContainer>
-        {favorites.map((pokemon, idx) => (
-          <PokeCard
-            pokemon={pokemon}
-            key={idx}
-            onMyFavsSection={onMyFavsSection}
-          />
-        ))}
+        {favorites &&
+          favorites.map((pokemon, idx) => (
+            <PokeCard pokemon={pokemon} key={idx} />
+          ))}
       </PokedexCardsContainer>
       <Footer />
     </PokedexContainer>
@@ -118,6 +112,17 @@ const PokeSearchContainer = styled.div`
   @media (min-width: 768.1px) {
     margin: 78px 150px 0px;
   }
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+`;
+
+const FilterText = styled.div`
+  color: black;
+  font-size: 18px;
+  line-height: 24px;
+  margin-right: 4px;
 `;
 
 const PokedexCardsContainer = styled.div`
